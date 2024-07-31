250066b06ca36c3ffb226789a9a90e2a57f864f4
GVAMDaniel <drodriguez@gvam.es>
2024-08-01 00:48:01 +0200

    feature: añadido intl para traduccion de app

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,36 @@
+import 'package:apptesting/presentation/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+
+  static void setLocale(BuildContext context, Locale newLocale) {
+    _MainAppState? state = context.findAncestorStateOfType<_MainAppState>();
+    state?.setLocale(newLocale);
+  }
+}
+
+class _MainAppState extends State<MainApp> {
+  Locale? _locale;
+
+  setLocale(Locale locale) {
+    setState(() => _locale = locale);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return MaterialApp(
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
+        locale: _locale,
+        home: const HomePage());
   }
 }
